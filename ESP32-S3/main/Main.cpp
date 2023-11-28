098b6ecba98ab01268cdb6502383b3b6b7acd378
jbilander <jorgen.bilander@gmail.com>
2023-11-28 21:50:53 +0100

    Added base skel of USB HID Host

--- ESP32-S3/main/Main.cpp
+++ ESP32-S3/main/Main.cpp
@@ -2,12 +2,17 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/semphr.h"
+#include "freertos/event_groups.h"
+#include "freertos/queue.h"
 #include "esp_timer.h"
 #include "esp_lcd_panel_ops.h"
 #include "esp_lcd_panel_rgb.h"
 #include "driver/gpio.h"
 #include "esp_err.h"
 #include "esp_log.h"
+#include "usb/usb_host.h"
+#include "usb/hid_host.h"
+#include "usb_hid_host.h"
 
 #define LCD_PIXEL_CLOCK_HZ (25 * 1000 * 1000)
 #define PIN_NUM_HSYNC 1   // New Line
@@ -38,15 +43,23 @@
 // FrameBuffer
 #define LCD_NUM_FB 2 // allocate double frame buffer, Maximum number of buffers are 3
 
-//extern "C" void lvgl_demo_ui(lv_disp_t *disp);
-
-static const char *TAG = "RGB_LCD";
-static uint8_t s_led_state = 0;
+static const char *LCD_TAG = "RGB_LCD";
+static const char *USB_TAG = "USB_HID";
 
 static esp_lcd_panel_handle_t panel_handle;
-//static lv_disp_t *disp;
-//static lv_disp_draw_buf_t disp_buf; // contains internal graphic buffer(s) called draw buffer(s)
-//static lv_disp_drv_t disp_drv;      // contains callback functions
+static uint8_t s_led_state = 0;
+
+// USB Host stuff
+QueueHandle_t app_event_queue;
+app_event_queue_t evt_queue;
+extern "C" QueueHandle_t create_queue(int queue_length, unsigned int item_size);
+extern "C" void hid_host_device_callback(hid_host_device_handle_t hid_device_handle,
+                                         const hid_host_driver_event_t event,
+                                         void *arg);
+
+// static lv_disp_t *disp;
+// static lv_disp_draw_buf_t disp_buf; // contains internal graphic buffer(s) called draw buffer(s)
+// static lv_disp_drv_t disp_drv;      // contains callback functions
 
 static void configure_led(void)
 {
@@ -56,7 +69,7 @@ static void configure_led(void)
 
 static void init_rgb_lcd(void)
 {
-    ESP_LOGI(TAG, "Install RGB LCD panel driver");
+    ESP_LOGI(LCD_TAG, "Install RGB LCD panel driver");
     esp_lcd_rgb_panel_config_t panel_config = {
         .clk_src = LCD_CLK_SRC_DEFAULT,
         .timings = {// The timing parameters should refer to your LCD spec
@@ -81,19 +94,98 @@ static void init_rgb_lcd(void)
         .flags = {.fb_in_psram = true}};
     ESP_ERROR_CHECK(esp_lcd_new_rgb_panel(&panel_config, &panel_handle));
 
-/*
-    ESP_LOGI(TAG, "Register event callbacks");
-    esp_lcd_rgb_panel_event_callbacks_t cbs = {
-        .on_vsync = on_vsync_event};
-    ESP_ERROR_CHECK(esp_lcd_rgb_panel_register_event_callbacks(panel_handle, &cbs, &disp_drv));
-*/
-    ESP_LOGI(TAG, "Initialize RGB LCD panel");
+    /*
+        ESP_LOGI(LCD_TAG, "Register event callbacks");
+        esp_lcd_rgb_panel_event_callbacks_t cbs = {
+            .on_vsync = on_vsync_event};
+        ESP_ERROR_CHECK(esp_lcd_rgb_panel_register_event_callbacks(panel_handle, &cbs, &disp_drv));
+    */
+    ESP_LOGI(LCD_TAG, "Initialize RGB LCD panel");
     ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
     ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
 }
 
+static void usb_lib_task(void *pvParameter)
+{
+    const usb_host_config_t host_config = {
+        .skip_phy_setup = false,
+        .intr_flags = ESP_INTR_FLAG_LEVEL1,
+    };
+
+    ESP_ERROR_CHECK(usb_host_install(&host_config));
+    xTaskNotifyGive((TaskHandle_t)pvParameter);
+
+    while (1)
+    {
+        uint32_t event_flags;
+        usb_host_lib_handle_events(portMAX_DELAY, &event_flags);
+        // usb_host_lib_handle_events(100, &event_flags);
+        //  In this example, there is only one client registered
+        //  So, once we deregister the client, this call must succeed with ESP_OK
+        if (event_flags & USB_HOST_LIB_EVENT_FLAGS_NO_CLIENTS)
+        {
+            ESP_LOGI(USB_TAG, "NO CLIENTS");
+            ESP_ERROR_CHECK(usb_host_device_free_all());
+            break;
+        }
+        if (event_flags & USB_HOST_LIB_EVENT_FLAGS_ALL_FREE)
+        {
+            ESP_LOGI(USB_TAG, "ALL FREE");
+        }
+    }
+
+    ESP_LOGI(USB_TAG, "USB shutdown");
+    // Clean up USB Host
+    vTaskDelay(10); // Short delay to allow clients clean-up
+    ESP_ERROR_CHECK(usb_host_uninstall());
+    vTaskDelete(NULL);
+}
+
+static void configure_usb_hid(void)
+{
+    ESP_LOGI(LCD_TAG, "Configure USB HID");
+    /*
+     * HID host driver configuration
+     * - create background task for handling low level event inside the HID driver
+     * - provide the device callback to get new HID Device connection event
+     */
+
+    const hid_host_driver_config_t hid_host_driver_config = {
+        .create_background_task = true,
+        .task_priority = 5,
+        .stack_size = 4096,
+        .core_id = tskNO_AFFINITY,
+        .callback = hid_host_device_callback,
+        .callback_arg = NULL};
+
+    ESP_ERROR_CHECK(hid_host_install(&hid_host_driver_config));
+
+    // Create queue to receive USB HID Events
+    app_event_queue = create_queue(10, sizeof(app_event_queue_t));
+    ESP_LOGI(USB_TAG, "Waiting for HID Device to be connected");
+
+    while (1)
+    {
+        if (xQueueReceive(app_event_queue, &evt_queue, portMAX_DELAY))
+        {
+            if (APP_EVENT_HID_HOST == evt_queue.event_group)
+            {
+                ESP_LOGI(USB_TAG, "APP_EVENT_HID_HOST");
+            }
+        }
+    }
+}
+
 extern "C" void app_main()
 {
+    ESP_LOGI(USB_TAG, "Create usb_lib_task to initialize USB Host library");
+    BaseType_t task_created = xTaskCreate(usb_lib_task, "usb_events", 4096, xTaskGetCurrentTaskHandle(), 2, NULL);
+    assert(task_created == pdTRUE);
+
+    // Wait for notification from usb_lib_task to proceed
+    ulTaskNotifyTake(false, 1000);
+
+    configure_usb_hid();
     init_rgb_lcd();
     configure_led();
 
