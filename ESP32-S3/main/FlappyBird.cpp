0780f62af0950cdc0b5d33581aa30ca22d2a580d
jbilander <jorgen.bilander@gmail.com>
2024-01-05 10:44:36 +0100

    Added bird animation

--- ESP32-S3/main/FlappyBird.cpp
+++ ESP32-S3/main/FlappyBird.cpp
@@ -2,20 +2,38 @@
 
 FlappyBird::FlappyBird()
 {
+    // Set background stuff
+    bg_img = lv_img_create(lv_scr_act());
+    lv_img_set_src(bg_img, &background_houses);
+    lv_style_set_bg_color(&bg_style, mountain_range_blue);
+    lv_obj_add_style(lv_scr_act(), &bg_style, LV_OPA_TRANSP);
+    bg_bottom_rect = lv_obj_create(lv_scr_act());
+    lv_obj_set_size(bg_bottom_rect, LCD_H_RES, 100);
+    lv_style_init(&style_rect);
+    lv_style_set_bg_color(&style_rect, lightish_green);
+    lv_style_set_radius(&style_rect, 0);
+    lv_style_set_border_width(&style_rect, 0);
+    lv_obj_add_style(bg_bottom_rect, &style_rect, LV_OPA_TRANSP);
+    lv_obj_align(bg_bottom_rect, LV_ALIGN_BOTTOM_LEFT, 0, 0);
+    lv_obj_align_to(bg_img, bg_bottom_rect, LV_ALIGN_OUT_TOP_LEFT, 0, 0);
+
+    // Bird animation
+    bird = lv_animimg_create(lv_scr_act());
+    lv_obj_align(bird, LV_ALIGN_CENTER, -150, 0);
+    lv_animimg_set_src(bird, (const void **)bird_anim_imgs, 3);
+    lv_animimg_set_duration(bird, 300);
+    lv_animimg_set_repeat_count(bird, LV_ANIM_REPEAT_INFINITE);
+    lv_animimg_start(bird);
+
+    section_list = {0, 0, 0, 0};
+    reset_game = true;
+    section_width = (float)LCD_V_RES / (float)(section_list.size() - 1);
 }
 
 FlappyBird::~FlappyBird()
 {
 }
 
-void FlappyBird::init()
-{
-    section_list = {0, 0, 0, 0};
-    reset_game = true;
-    section_width = (float)LCD_V_RES / (float)(section_list.size() - 1);
-}
-
 void FlappyBird::onUpdate()
 {
-    ESP_LOGI("FLAP:", "FLAPPING");
 }
--- ESP32-S3/main/FlappyBird.h
+++ ESP32-S3/main/FlappyBird.h
@@ -2,33 +2,50 @@
 #define FLAPPYBIRD_H_FILE
 
 #include <esp_log.h>
+#include <esp_timer.h>
 #include <list>
+#include <lvgl.h>
 #include "Main.h"
+
+// Background components
+LV_IMG_DECLARE(background_houses);
+static lv_img_dsc_t background_img = background_houses;
+static lv_color_t mountain_range_blue = {.ch = {.blue = 26, .green = 48, .red = 9}};
+static lv_color_t lightish_green = {.ch = {.blue = 14, .green = 57, .red = 11}};
+static lv_style_t bg_style;
+static lv_obj_t *bg_img;
+static lv_obj_t *bg_bottom_rect;
+static lv_style_t style_rect;
+
+// Birdie
+LV_IMG_DECLARE(flappy1)
+LV_IMG_DECLARE(flappy2)
+LV_IMG_DECLARE(flappy3)
+
+static const void *bird_anim_imgs[3] = {
+    &flappy1,
+    &flappy2,
+    &flappy3,
+};
 
 class FlappyBird
 {
 private:
-    // float fBirdPosition = 0.0f;
-    // float fBirdVelocity = 0.0f;
-    // float fBirdAcceleration = 0.0f;
-    // float fGravity = 100.0f;
-    // float fLevelPosition = 0.0f;
+    lv_obj_t *bird;
+    lv_obj_t *img_flappy1;
+    lv_obj_t *img_flappy2;
+    lv_obj_t *img_flappy3;
 
     float section_width;
     std::list<int> section_list;
 
-    // bool bHasCollided = false;
+    bool has_collided = false;
     bool reset_game = false;
-
-    // int nAttemptCount = 0;
-    // int nFlapCount = 0;
-    // int nMaxFlapCount = 0;
 
 protected:
 public:
     FlappyBird();
     ~FlappyBird();
-    void init();
     void onUpdate();
 };
 
